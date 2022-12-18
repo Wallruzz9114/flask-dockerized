e5484ef3a245b253f8833e3d8aa2688c52d93728
Wallruzz9114 <josepinto@Joses-MacBook-Pro.local>
2022-12-18 15:39:06 -0700

    Added Swagger for api documentation

--- src/__init__.py
+++ src/__init__.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask
 from flask_admin import Admin
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 db = SQLAlchemy()
 admin = Admin(template_mode="bootstrap3")
@@ -11,6 +12,7 @@ admin = Admin(template_mode="bootstrap3")
 def create_app(script_info=None):
     # Instantiate the app
     app = Flask(__name__)
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1)
 
     # set config
     app_settings = os.getenv("APP_SETTINGS")
@@ -22,13 +24,10 @@ def create_app(script_info=None):
     if os.getenv("FLASK_ENV") == "development":
         admin.init_app(app)
 
-    # Register blueprints
-    from src.api.ping import ping_blueprint
+    # register api
+    from src.api import api
 
-    app.register_blueprint(ping_blueprint)
-    from src.api.users.views import users_blueprint
-
-    app.register_blueprint(users_blueprint)
+    api.init_app(app)
 
     # Shell context for flask cli
     @app.shell_context_processor
--- src/api/users/views.py
+++ src/api/users/views.py
@@ -1,13 +1,18 @@
-from flask import Blueprint, request
-from flask_restx import Api, Resource, fields
+from flask import request
+from flask_restx import Namespace, Resource, fields
 
-from src.api.users.crud import (create_user, delete_user, get_all_users,
-                                get_user_by_email, get_user_by_id, update_user)
+from src.api.users.crud import (  # isort:skip
+    create_user,
+    delete_user,
+    get_all_users,
+    get_user_by_email,
+    get_user_by_id,
+    update_user,
+)
 
-users_blueprint = Blueprint("users", __name__)
-api = Api(users_blueprint)
+users_namespace = Namespace("users")
 
-user = api.model(
+user = users_namespace.model(
     "User",
     {
         "id": fields.Integer(readOnly=True),
@@ -19,11 +24,13 @@ user = api.model(
 
 
 class UsersList(Resource):
-    @api.marshal_with(user, as_list=True)
+    @users_namespace.marshal_with(user, as_list=True)
     def get(self):
         return get_all_users(), 200
 
-    @api.expect(user, validate=True)
+    @users_namespace.expect(user, validate=True)
+    @users_namespace.response(201, "<user_email> was added")
+    @users_namespace.response(400, "Sorry. That email already exists.")
     def post(self):
         post_data = request.get_json()
         username = post_data.get("username")
@@ -43,16 +50,21 @@ class UsersList(Resource):
 
 
 class Users(Resource):
-    @api.marshal_with(user)
+    @users_namespace.marshal_with(user)
+    @users_namespace.response(200, "Success")
+    @users_namespace.response(404, "User <user_id> does not exist")
     def get(self, user_id: int):
         user = get_user_by_id(user_id)
 
         if not user:
-            api.abort(404, f"User {user_id} does not exist")
+            users_namespace.abort(404, f"User {user_id} does not exist")
 
         return user, 200
 
-    @api.expect(user, validate=True)
+    @users_namespace.expect(user, validate=True)
+    @users_namespace.response(200, "<user_id> was updated!")
+    @users_namespace.response(400, "Sorry. That email already exists.")
+    @users_namespace.response(404, "User <user_id> does not exist")
     def put(self, user_id):
         post_data = request.get_json()
         username = post_data.get("username")
@@ -62,7 +74,7 @@ class Users(Resource):
         user = get_user_by_id(user_id)
 
         if not user:
-            api.abort(404, f"User {user_id} does not exist")
+            users_namespace.abort(404, f"User {user_id} does not exist")
 
         if get_user_by_email(email):
             response_object["message"] = "Sorry. That email already exists."
@@ -73,12 +85,14 @@ class Users(Resource):
         response_object["message"] = f"{user.id} was updated!"
         return response_object, 200
 
+    @users_namespace.response(200, "<user_id> was removed!")
+    @users_namespace.response(404, "User <user_id> does not exist")
     def delete(self, user_id):
         response_object = {}
         user = get_user_by_id(user_id)
 
         if not user:
-            api.abort(404, f"User {user_id} does not exist")
+            users_namespace.abort(404, f"User {user_id} does not exist")
 
         delete_user(user)
 
@@ -86,5 +100,5 @@ class Users(Resource):
         return response_object, 200
 
 
-api.add_resource(UsersList, "/users")
-api.add_resource(Users, "/users/<int:user_id>")
+users_namespace.add_resource(UsersList, "")
+users_namespace.add_resource(Users, "/<int:user_id>")
