fbf512938626a8303a93f729fc859d5b9c05eca3
Wallruzz9114 <josepinto@Joses-MacBook-Pro.local>
2022-12-18 19:27:36 -0700

    Updated react frontend with typescript, linting and code formatting

---
+++ services/website/src/App.tsx
@@ -0,0 +1,16 @@
+const App = (): JSX.Element => (
+  <section className="section">
+    <div className="container">
+      <div className="columns">
+        <div className="column is-one-third">
+          <br />
+          <h1 className="title is-1 is-1">Users</h1>
+          <hr />
+          <br />
+        </div>
+      </div>
+    </div>
+  </section>
+);
+
+export default App;
